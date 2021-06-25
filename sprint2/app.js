129d12ac9cd04829ae903f812297aee090358880
Cristianlell@gmail.com <cristianlell24@gmail.com>
2021-06-24 23:30:23 -0300

    modifico la ruta registro en app.js y diseño registrarse para escritorio

--- sprint2/app.js
+++ sprint2/app.js
@@ -9,8 +9,8 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "index.html"))
 );
 
-app.get("/registro", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "registro.html"))
+app.get("/registrarse", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "registrarse.html"))
 );
 // app.get("/login", (req, res) =>
 //   res.sendFile(path.join(__dirname, "views", "login.html"))
