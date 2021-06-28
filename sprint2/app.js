5c3791ba21b0dd830ee8cc7204aed9567578353f
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-06-28 10:28:44 -0300

    trabajo responsive desktop en carrito

--- sprint2/app.js
+++ sprint2/app.js
@@ -9,7 +9,6 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "index.html"))
 );
 
-<<<<<<< HEAD
 app.get("/carrito", (req, res) =>
   res.sendFile(path.resolve(__dirname, "views" , "carrito.html"))
 );
@@ -17,14 +16,12 @@ app.get("/descripcion-producto", (req, res) =>
   res.sendFile(path.resolve(__dirname, "views", "descripcion-producto.html"))
 );
 
-=======
 app.get("/registro", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "registro.html"))
 );
 // app.get("/login", (req, res) =>
 //   res.sendFile(path.join(__dirname, "views", "login.html"))
 // );
->>>>>>> 0b0dfefd5687ad0e12cc1cd418882f1e5bcc0af9
 app.listen(port, () =>
   console.log("Server runing in http://localhost:" + port)
 );
