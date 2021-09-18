ea8ea3fd8ba0bbb39ba7d5f39ae200d95ac4cbfd
Cristianlell <cristianlell24@gmail.com>
2021-09-18 16:48:59 -0300

    creo migraciones

---
+++ sitio/src/database/models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Cart,{
+        as : 'cart'
+      })
+      User.hasMany(models.Order,{
+        as : 'order'
+      })
+    }
+  };
+  User.init({
+    nameUser: DataTypes.STRING(45),
+    password: DataTypes.STRING(100),
+    firstName: DataTypes.STRING(100),
+    email: DataTypes.STRING(45),
+    avatar: DataTypes.STRING,
+    rol: DataTypes.STRING(45),
+    country: DataTypes.STRING(45),
+    city: DataTypes.STRING(45),
+    favoriteAvenger: DataTypes.STRING(45),
+    hobbies: DataTypes.STRING(250)
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
