af0b95f02afcf38390fb8d7b24cd1a60a875d541
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-09-27 19:33:27 -0300

    variable de entorno

--- sitio/src/database/config/config.js
+++ sitio/src/database/config/config.js
@@ -1,23 +1,24 @@
+require("dotenv").config();
 module.exports = {
   development: {
-    username: "root",
-    password: '',
-    database: "torreMarvel_db",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql"
-  }
-}
+    dialect: "mysql",
+  },
+};
