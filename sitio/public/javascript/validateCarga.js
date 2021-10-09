6f8fa284db9ce764d982d19f4eeba3b53647cd7e
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-10-09 16:14:15 -0300

    Validacion de carga producto

--- sitio/public/javascript/validateCarga.js
+++ sitio/public/javascript/validateCarga.js
@@ -3,15 +3,27 @@ const $ = id =>document.querySelector(id);
 
 
 
-let regExEmail =  /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
-let regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
-
-
 window.addEventListener('load', (e) => {
     console.log('registerValidator connected success');
 e.preventDefault()
 
-let imagen = $("#imagenProducto")
+
+let nombre = $("#nombreProducto")
+nombre.addEventListener("blur", () => {
+  if (!nombre.value.trim()) {
+  nombre.classList.add("is-invalid");
+    $(".errorName").innerHTML = "El nombre es obligatorio";
+  } else if(nombre.value.length<3){
+     nombre.classList.add("is-invalid");
+     $(".errorName").innerHTML = "El nombre debe tener al menos 3 letras";
+   } else {
+    nombre.classList.remove("is-invalid");
+    nombre.classList.add("is-valid");
+    $(".errorName").innerHTML = null;
+  }
+});
+
+let imagen = $("#imagenProducto");
 imagen.addEventListener("focus", () => {
   $(".errorImagen").innerHTML =
     "Las extensiones aceptadas son: .jpg, .npg, .img";
@@ -19,28 +31,20 @@ imagen.addEventListener("focus", () => {
 
 imagen.addEventListener("blur", () => {
   if (!imagen.value.trim()) {
-  imagen.classList.add("is-invalid");
-    $(".errorImagen").innerHTML = "El nombre es obligatorio";
+    imagen.classList.add("is-invalid");
+    $(".errorImagen").innerHTML = "Debes subir al menos una imagen";
   } else {
-   imagen.classList.remove("is-invalid");
-   imagen.classList.add("is-valid");
+    imagen.classList.remove("is-invalid");
+    imagen.classList.add("is-valid");
     $(".errorImagen").innerHTML = null;
-  }
-});
-let nombre = $("#nombreProducto")
-nombre.addEventListener("blur", () => {
-  if (!nombre.value.trim()) {
-  nombre.classList.add("is-invalid");
-    $(".errorName").innerHTML = "El nombre es obligatorio";
-  } else {
-    nombre.classList.remove("is-invalid");
-    nombre.classList.add("is-valid");
-    $(".errorName").innerHTML = null;
   }
 });
 let precio= $("#precioProducto")
  precio.addEventListener("blur", () => {
-   if (precio.value <100) {
+     if (!precio.value.trim()) {
+  precio.classList.add("is-invalid");
+    $(".errorPrecio").innerHTML = "Pensas regalar este producto? Ponele Precio!";
+     }else if (precio.value <=100) {
      precio.classList.add("is-invalid");
      $(".errorPrecio").innerHTML = "Debes ingresar un precio mayor a 100";
    } else {
@@ -61,6 +65,16 @@ let precio= $("#precioProducto")
      $(".errorDesc").innerHTML = "";
    }
  });
- 
+ let descripcion = $("#descripcion");
+ descripcion.addEventListener("blur", () => {
+   if (descripcion.value.length<10) {
+     descripcion.classList.add("is-invalid");
+     $(".errorDescripcion").innerHTML = "Por favor amplia tu descripcion";
+   } else {
+     descripcion.classList.remove("is-invalid");
+     descripcion.classList.add("is-valid");
+     $(".errorDescripcion").innerHTML = "";
+   }
+ });
 
 })
