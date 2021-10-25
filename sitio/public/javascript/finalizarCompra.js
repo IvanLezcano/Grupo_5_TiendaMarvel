ddded04b917b968e9d80b3d3d13325243b1bb714
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-10-25 18:32:29 -0300

    carga productos

--- sitio/public/javascript/finalizarCompra.js
+++ sitio/public/javascript/finalizarCompra.js
@@ -1,6 +1,7 @@
 console.log('hola mundo');
 
 let contenedor= document.querySelector('.contenedor')
+let spantotal= document.querySelector('.total')
 
 let productos= localStorage.getItem('producto')
 productos= JSON.parse(productos)
@@ -9,28 +10,43 @@ productos.forEach(producto => {
     console.log(producto);
 });
 
-productos.map(producto => {
-    
-    contenedor.innerHTML = `
+var total= 0;
+productos.forEach((producto) => {
+  producto.forEach((item) => {
+  
+  total += item.total;
+}); 
+}); 
+console.log(total);
+
+ spanTotal.innerHTML += `$ ${total}`
+
+
+productos.forEach((producto) => {
+  producto.forEach((item) => {
+      
+    contenedor.innerHTML += `
      <article class="carrito-main-article">
                             <div class="carrito-main-producto">
-                                <div class="carrito-main-producto-imagen"><img src="/images/merchandising/${producto.imagen}" alt=""></div>
+                                <div class="carrito-main-producto-imagen"><img src="/images/merchandising/${item.imagen}" alt=""></div>
                                 <div class="carrito-main-producto-descripcion">
                                     <div>
 
-                                        <p id="nombrecito">${producto.nombre} </p>
+                                        <p id="nombrecito">${item.nombre} </p>
                                         <p>Entregado por Torre Marvel</p>
-                                        <p></p>
+                                        <p>Cantidad ${item.cantidad}</p>
                                     </div>
-                                    <!-- <div>
-                                    <p><a href="">Eliminar producto</a> </p>
-                                </div> -->
+                                   
                                 </div>
                             </div>
                             <div class="carrito-main-article-precio">
                                 <div class="carrito-main-article-precio-subrayado">
                                     <p>Precio unitario:</p>
-                                    <Span class="precioUnitario">$ ${producto.precio}</Span>
+                                    <Span class="precioUnitario">$ ${item.precio}</Span>
+                                </div>
+                                <div class="carrito-facturacion-cobro-subtotal">
+                                    <p>Subtotal</p>
+                                    <span class="subTotal">$ ${item.total}</span>
                                 </div>
                             </div>
                         </article>
@@ -39,27 +55,20 @@ productos.map(producto => {
 
                         <section class="carrito-facturacion">
                             <div class="carrito-facturacion-tarjetas">
-                                <!--  <div class="carrito-facturacion-tarjetas-descuento">
+                                <div class="carrito-facturacion-tarjetas-descuento">
                                 <h3>Codigo de descuento</h3>
                                 <form action=""><input type="text" placeholder="Ingrese codigo"></form>
-                            </div> -->
+                            </div> 
                                 <div class="carrito-facturacion-tarjetas-descuento-imagen">
                                     <img src="/images/tarjetas.PNG" alt="">
                                 </div>
                             </div>
 
                             <div class="carrito-facturacion-cobro">
-                                <div class="carrito-facturacion-cobro-subtotal">
-                                    <p>Subtotal</p>
-                                    <span class="subTotal">$ ${producto.precio}</span>
-                                </div>
-                                <div class="carrito-facturacion-cobro-total-general">
-                                    <div class="carrito-facturacion-cobro-total-general-i">
-                                        <span class="carrito-facturacion-cobro-total-general-i-precio">Total</span>
-                                        <span class="total">$ ${producto.total}</span>
-                                    </div>
-                                </div>
+                                
+                               
                            </div>
 
     `;
-});
+  });
+})
