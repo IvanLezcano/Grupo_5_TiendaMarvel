cbcb4b372e8a2b5472959e9703af54a2d2549bd4
Cristianlell <cristianlell24@gmail.com>
2021-10-28 15:43:42 -0300

    logo-header arreglo tamaño de imagen

--- sitio/src/database/config/config.js
+++ sitio/src/database/config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   development: {
     username: 'root',
-    password:'',
+    password:'password',
     database: 'torreMarvel_db',
     host: "127.0.0.1",
     dialect: "mysql",
