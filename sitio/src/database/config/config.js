e1b9a7209528f547982e0a449475f88855b7c07c
EzeBauer <83975962+EzeBauer@users.noreply.github.com>
2021-10-28 16:01:29 -0300

    .env

--- sitio/src/database/config/config.js
+++ sitio/src/database/config/config.js
@@ -1,9 +1,9 @@
-/* require("dotenv").config(); */
+require("dotenv").config(); 
 module.exports = {
   development: {
-    username: 'root',
-    password:'password',
-    database: 'torreMarvel_db',
+    username:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
     host: "127.0.0.1",
     dialect: "mysql",
   },
